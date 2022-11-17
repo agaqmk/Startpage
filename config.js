34ad697c2451802993df7c384d01f244d38efb12
agaqmk <116126713+agaqmk@users.noreply.github.com>
2022-11-16 21:51:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
-	imageBackground: false,
+	name: 'aga',
+	imageBackground: true,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
